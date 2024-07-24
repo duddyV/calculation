6bc4525bf8f237ef4f493b0043dfd1562ed4b2e9
Kyrylo Dudka <admin@dudbook-pro.local>
2024-07-24 22:57:19 +0300

    v1.0.1

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
+// Package main provides a simple example of adding two integers using the Add function.
 package main
 
 import "fmt"
+
+// Add takes two integers a and b and returns their sum.
+// For more information on addition, see https://www.mathsisfun.com/numbers/addition.html.
+func Add(a, b int) int {
+	return a + b
+}
 
 func main() {
 	a := 3
 	b := 4
 
 	fmt.Println(Add(a, b))
-}
-
-func Add(a, b int) int {
-	return a + b
 }
