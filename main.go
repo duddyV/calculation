53fe55b558079d0cdd10fb5dcef0f15404f484ba
Kyrylo Dudka <admin@dudbook-pro.local>
2024-07-24 23:05:51 +0300

    v2.0.0

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 // Package main provides a simple example of adding two integers using the Add function.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
 
 // Add takes two integers a and b and returns their sum.
 // For more information on addition, see https://www.mathsisfun.com/numbers/addition.html.
-func Add(a, b int) int {
+func Add[T Number] (a, b T) T {
 	return a + b
 }
 
 func main() {
-	a := 3
-	b := 4
+	a := 33.1
+	b := 4.37
+	c := 4
+	d := 111
 
 	fmt.Println(Add(a, b))
+	fmt.Println(Add(c, d))
 }
